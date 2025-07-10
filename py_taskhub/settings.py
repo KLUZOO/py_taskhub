4de5da1ef1e73fa56c59ad54cc1ac0982a34e273
Sasha <kluzodota@gmail.com>
2025-07-10 13:39:09 +0300

    set up the paths щof urls.py

--- py_taskhub/settings.py
+++ py_taskhub/settings.py
@@ -101,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "tasks.Worker"
 
+LOGIN_REDIRECT_URL = "/"
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -116,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
