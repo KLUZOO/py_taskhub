726387309fa468ac9c205f289fe3358b34420591
Sasha <kluzodota@gmail.com>
2025-07-10 19:03:39 +0300

    created a blank singup page and connected it

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 
 from .views import (
-    TaskList,
-    WorkerList,
-    TaskTypeList,
-    PositionList,
+    TaskListView,
+    WorkerListView,
+    TaskTypeListView,
+    PositionListView,
+    WorkerCreateView,
 )
 
 urlpatterns = [
-    path("", TaskList.as_view(), name="task_list"),
-    path("worker/", WorkerList.as_view(), name="worker_list"),
-    path("task-type/", TaskTypeList.as_view(), name="task_type_list"),
-    path("position/", PositionList.as_view(), name="position_list"),
+    path("", TaskListView.as_view(), name="task_list"),
+    path("worker/", WorkerListView.as_view(), name="worker_list"),
+    path("task-type/", TaskTypeListView.as_view(), name="task_type_list"),
+    path("position/", PositionListView.as_view(), name="position_list"),
+    path("accounts/singup/", WorkerCreateView.as_view(), name="singup_create"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -4,24 +4,30 @@ from django.views import generic
 from tasks.models import Task, Worker, TaskType, Position
 
 
-class TaskList(generic.ListView):
+class TaskListView(generic.ListView):
     model = Task
     fields = "__all__"
     template_name = "tasks/index.html"
 
 
-class WorkerList(generic.ListView):
+class WorkerListView(generic.ListView):
     model = Worker
     fields = "__all__"
     template_name = "tasks/worker_list.html"
 
 
-class TaskTypeList(generic.ListView):
+class TaskTypeListView(generic.ListView):
     model = TaskType
     fields = "__all__"
     template_name = "tasks/task_type_list.html"
 
-class PositionList(generic.ListView):
+class PositionListView(generic.ListView):
     model = Position
     fields = "__all__"
     template_name = "tasks/position_list.html"
+
+
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+    template_name = "registration/signup.html"
+    fields = "__all__"
