d816c67d6a878d4d0b321947728d909383d5373b
Sasha <kluzodota@gmail.com>
2025-07-10 17:53:44 +0300

    added a blank page for worker and included it in the relevant links

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (
-    TaskList
+    TaskList,
+    WorkerList,
 )
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task_list"),
+    path("worker/", WorkerList.as_view(), name="worker_list"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 
-from tasks.models import Task
+from tasks.models import Task, Worker
 
 
 class TaskList(generic.ListView):
     model = Task
     fields = "__all__"
     template_name = "tasks/index.html"
+
+class WorkerList(generic.ListView):
+    model = Worker
+    fields = "__all__"
+    template_name = "tasks/worker_list.html"
