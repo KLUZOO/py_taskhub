c61e279f65cf0fa07b5724eb01510c986c7571d6
Sasha <kluzodota@gmail.com>
2025-07-10 13:28:12 +0300

    registered models in the admin area

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Worker, Position, TaskType, Task
+
+
+@admin.register(Worker)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Position)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(TaskType)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Task)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name", "priority", "task_type")
