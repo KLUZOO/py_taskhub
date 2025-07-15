6883527dc5cedcfc1162f8401559a90765ff9402
Sasha <kluzodota@gmail.com>
2025-07-15 13:04:09 +0300

    configure some settings

--- py_taskhub/settings/prod.py
+++ py_taskhub/settings/prod.py
@@ -4,6 +4,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -17,4 +21,3 @@ DATABASES = {
         'PORT': int(os.environ['POSTGRES_DB_PORT']),
     }
 }
-
